08ec552ca1741b4a4fd1e4fd054acbaa7f917b08
dhruv <88879715+dk2101@users.noreply.github.com>
2021-08-19 01:06:40 +0530

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,28 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart'
-    show BuildContext, Key, MaterialApp, StatelessWidget, Widget, runApp;
+    show BuildContext, MaterialApp, StatelessWidget, Widget, runApp;
 import 'package:flutter/material.dart';
 
 import 'package:time_table/pages/home_page.dart';
 import 'package:time_table/pages/login_page.dart';
 
-
 void main() {
-  // ignore: prefer_const_constructors
   runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-
-
-
-        
-      ),
+      theme: ThemeData(),
       initialRoute: "/",
-
       routes: {
         "/": (context) => const LoginPage(),
         "/home": (context) => const HomePage(),
-        
       },
     );
   }
-}
-
-class GoogleFonts {
 }
---
+++ lib/utils/routes.dart
@@ -0,0 +1,6 @@
+class MyRoutes{
+  static String loginRouts = "/login";
+  static String homenRout = "/home";
+
+  
+}
